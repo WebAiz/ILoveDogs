6b83ae871b1e514b9387c318b37525011d83df9b
aizada <aizada.turarova@gmail.com>
2024-04-16 16:23:03 +0500

    feat: add loader animation

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,12 +2,14 @@
 import * as React from "react";
 import Image from "next/image";
 import loader from "@/assets/gifs/loading_white.gif";
+import { Modal } from "@mui/base";
+import styles from "./Loader.module.scss";
 
 type Props = {};
 
 export function Loader(props: Props) {
   return (
-    <div>
+    <div className={styles.loader}>
       <Image src={loader} alt={"load"} />
     </div>
   );
